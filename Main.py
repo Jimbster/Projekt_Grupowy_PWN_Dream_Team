c99645e4225ee8c6d28a27c7704322eca635ea7f
Jimbster <jimbex@wp.pl>
2020-05-09 16:25:22 +0200

    Pierwsza merytoryka3

--- Main.py
+++ Main.py
@@ -9,17 +9,24 @@ pd.set_option('display.max_columns', 20)
 
 
 #otwarcie pliku i pobranie danych
-data = pd.read_csv("Adult_train.tab", sep='\t', header=2)
+#data = pd.read_csv("Adult_train.tab", sep='\t', header=2)
+data = pd.read_csv("Adult_train.tab", sep='\t')
 # print(data)
 # print(data.isnull().sum())
 # print(data.shape)
 # print(data.head())
-print(data.describe(include=[pd.np.object]))
-print(data.dtypes)
+#print(data.describe(include=[pd.np.object]))
+#print(data.dtypes)
+
+data = data.replace('?',np.nan)
+#print(data.isnull().sum())
+#print(data.shape)
+#print(data.head())
+print(data.corr())
 
 #oczyszczanie danych
 
-
+# do wywalenia - education, native country, relationship
 #utworzenie tabeli bazy danych
 
 
