b5eb08a0132371f64ccc624948a662452a6feebb
Jimbster <jimbex@wp.pl>
2020-05-09 15:43:07 +0200

    Pierwsza merytoryka2

--- Main.py
+++ Main.py
@@ -2,12 +2,20 @@
 
 import pandas as pd
 import sklearn as sklearn
+import numpy as np
+
+pd.set_option('display.max_columns', 20)
+#pd.set_option('display.max_rows', None)
 
 
 #otwarcie pliku i pobranie danych
 data = pd.read_csv("Adult_train.tab", sep='\t', header=2)
-print(data)
-
+# print(data)
+# print(data.isnull().sum())
+# print(data.shape)
+# print(data.head())
+print(data.describe(include=[pd.np.object]))
+print(data.dtypes)
 
 #oczyszczanie danych
 
