be2629d62bb67bc536e9967f53aa6ff1cb7d6d32
Jimbster <jimbex@wp.pl>
2020-05-09 15:23:30 +0200

    Pierwsza merytoryka

--- Main.py
+++ Main.py
@@ -1,7 +1,18 @@
-"""""
-Poczatek projektu
-"""""
+#importy
 
-'''
-test
-'''
+import pandas as pd
+import sklearn as sklearn
+
+
+#otwarcie pliku i pobranie danych
+data = pd.read_csv("Adult_train.tab", sep='\t', header=2)
+print(data)
+
+
+#oczyszczanie danych
+
+
+#utworzenie tabeli bazy danych
+
+
+#podzial zbbioru na czesc treningowa i testowa
