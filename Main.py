7dd8aac5ee4f65b39a5f85855d0260c0820b4f7c
Jimbster <jimbex@wp.pl>
2020-05-09 16:35:57 +0200

    Czyste dane (skasowane wiersze z ?) + usunięte kolumny uznane za niepotrzenbe

--- Main.py
+++ Main.py
@@ -5,27 +5,26 @@ import sklearn as sklearn
 import numpy as np
 
 pd.set_option('display.max_columns', 20)
-#pd.set_option('display.max_rows', None)
-
 
 #otwarcie pliku i pobranie danych
-#data = pd.read_csv("Adult_train.tab", sep='\t', header=2)
-data = pd.read_csv("Adult_train.tab", sep='\t')
-# print(data)
-# print(data.isnull().sum())
-# print(data.shape)
-# print(data.head())
-#print(data.describe(include=[pd.np.object]))
-#print(data.dtypes)
 
+data = pd.read_csv("Adult_train.tab", sep='\t')
+
+#zamiana ? na wartosci nan i wyrzucenie wierszy ktore je zawieraja
 data = data.replace('?',np.nan)
 print(data.isnull().sum())
 print(data.corr())
 data = data.dropna(how='any')
 print(data.isnull().sum())
+
+
 #oczyszczanie danych
 
+print(data)
+data = data.drop(labels=["education","native-country","relationship"],axis=1)
+print(data.describe)
 # do wywalenia - education, native country, relationship
+
 #utworzenie tabeli bazy danych
 
 
