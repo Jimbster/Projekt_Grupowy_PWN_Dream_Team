42f76f2b257ed303b51b1ad8f81c061da547d940
Jimbster <jimbex@wp.pl>
2020-05-10 12:27:04 +0200

    Update Klasyfikatora

---
+++ ML_majority_voting_script_main.py
@@ -0,0 +1,42 @@
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.tree import DecisionTreeClassifier
+
+import Main
+import ML_algo_testing_main
+
+
+X_train= Main.X_train
+X_test= Main.X_test
+y_train= Main.y_train
+y_test= Main.y_test
+
+def ensableClassifier(clfs, X_train, X_test, y_train):
+    y_preds = []
+    # trenowanie i testowanie wszystkich klasyfikatorów z listy clfs
+    for clf in clfs:
+        clf.fit(X_train, y_train)
+        y_preds.append(clf.predict(X_test))
+    # głosowanie większościowe
+    y_result = y_preds[0]
+    clf_index = 1
+    while (clf_index < len(y_preds)):
+        index = 0
+        while (index < len(y_result)):
+            y_result[index] = y_result[index] + y_preds[clf_index][index]
+            index += 1
+        clf_index += 1
+    # uśrednianie i zaokrąglanie
+    for index, y in enumerate(y_result):
+        y_result[index] = round(y_result[index] / len(clfs))
+    return y_result
+
+y_pred_ensable_train = ensableClassifier(
+    [RandomForestClassifier(),KNeighborsClassifier(n_neighbors=5), DecisionTreeClassifier()], X_train, X_train, y_train)
+y_pred_ensable_test = ensableClassifier(
+    [RandomForestClassifier(),KNeighborsClassifier(n_neighbors=5), DecisionTreeClassifier()], X_train, X_test, y_train)
+
+print(y_pred_ensable_train)
+print(y_pred_ensable_test)
+
+ML_algo_testing_main.getClassificationScore("Ensemble",y_test,y_pred_ensable_test)
