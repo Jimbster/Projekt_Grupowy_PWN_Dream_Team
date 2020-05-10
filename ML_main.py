dacccebe257765bc705fd363d6afc5091bd85d57
Jimbster <jimbex@wp.pl>
2020-05-10 11:02:06 +0200

    4 Modele predykcyjne SVM daje nieprzewidywalne wyniki. W przypadku gdy nie ogranicza się ilości iteracji, zapętla się

--- ML_main.py
+++ ML_main.py
@@ -1,5 +1,5 @@
 from sklearn.ensemble import RandomForestClassifier
-from sklearn.metrics import accuracy_score, confusion_matrix
+from sklearn.metrics import accuracy_score, confusion_matrix, f1_score, precision_score
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.svm import SVC
 from sklearn.tree import DecisionTreeClassifier
@@ -24,8 +24,13 @@ def trainAndTestClassifier(clf, X_train, X_test, y_train):
 
 def getClassificationScore(clf_name ,y_test, y_pred):
     print("Nazwa klasyfikatora: " + clf_name)
-    print(accuracy_score(y_test, y_pred))
-    print(confusion_matrix(y_test, y_pred))
+    print("Dokładność klasyfikacji: " + str(accuracy_score(y_test, y_pred)))
+    print("Macierz konfuzji: " + str(confusion_matrix(y_test, y_pred)))
+    print("F1 :" + str(f1_score(y_test, y_pred)))
+    print("Precyzja: "+str(precision_score(y_test, y_pred)))
+    #czułość i specyficzność do zaimplementowania
+
+
 
 y_pred_Knn = trainAndTestClassifier(KNeighborsClassifier(n_neighbors=5),X_train,X_test, y_train)
 getClassificationScore("Knn", y_test, y_pred_Knn)
@@ -45,11 +50,3 @@ getClassificationScore("SVC", y_test, y_pred_svm_rbf_train)
 
 
 
-# y_pred_knn5_train = c.trainAndTestClassifier(KNeighborsClassifier(n_neighbors=5), X_train,X_train,y_train)
-# y_pred_knn5_test = c.trainAndTestClassifier(KNeighborsClassifier(n_neighbors=5), X_train,X_test,y_train)
-# y_pred_tree_train = c.trainAndTestClassifier(DecisionTreeClassifier(), X_train,X_train,y_train)
-# y_pred_tree_test = c.trainAndTestClassifier(DecisionTreeClassifier(), X_train,X_test,y_train)
-# y_pred_svm_lin_train = c.trainAndTestClassifier(SVC(kernel='linear'), X_train,X_train,y_train)
-# y_pred_svm_lin_test = c.trainAndTestClassifier(SVC(kernel='linear'), X_train,X_test,y_train)
-# y_pred_svm_rbf_train = c.trainAndTestClassifier(SVC(kernel='rbf', gamma='auto'), X_train,X_train,y_train)
-# y_pred_svm_rbf_test = c.trainAndTestClassifier(SVC(kernel='rbf', gamma='auto'), X_train,X_test,y_train)
